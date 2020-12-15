ab8c472af74af7b8bdb13cea38631c66f14b6a4b
yujie.zhang <2249692640@qq.com>
2020-12-15 17:53:21 +0800

    页面UI。

---
+++ app/src/main/java/com/example/cardbossapp2/MainActivity2.java
@@ -0,0 +1,17 @@
+package com.example.cardbossapp2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * 主页面
+ */
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
